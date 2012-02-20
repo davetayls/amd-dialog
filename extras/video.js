ac941e6c6af3fd15a187ede1450e7ff3847c9df3
davetayls <dave@the-taylors.org>
2012-02-20 22:18:17 +0000

    added extra to show vimeo player inside dialog

---
+++ extras/video.js
@@ -0,0 +1,44 @@
+/*global define, require */
+define(
+	[
+	'jquery',
+	'dialog'
+	],
+	function($, dialog){
+		'use strict';
+
+		return {
+			videoTemplate: [
+			'<iframe src="{{url}}"',
+			'width="{{width}}"',
+			'height="{{height}}"',
+			'frameborder="0"',
+			'webkitAllowFullScreen',
+			'mozallowfullscreen',
+			'allowFullScreen></iframe>'].join(' '),
+
+			getVimeoId: function(url) {
+				var matches = /vimeo\.com\/(\d*)/i.exec(url);
+				if (matches.length === 2) {
+					return matches[1];
+				} else {
+					return null;
+				}
+			},
+			getVimeoPlayerUrl: function(vimeoId) {
+				return 'http://player.vimeo.com/video/' + vimeoId;
+			},
+			init: function(){
+				var self = this;
+				dialog.init();
+				$('body').delegate('a[href*="vimeo.com"]', 'click', function(e) {
+					var vimeoId = self.getVimeoId(this.href),
+						playerUrl = self.getVimeoPlayerUrl(vimeoId)
+					;
+					dialog.showDialogType(playerUrl, 'iframe', {
+						iframeTemplate: self.videoTemplate
+					});
+				});
+			}
+		};
+	});
---
+++ test/spec/videoSpec.js
@@ -0,0 +1,75 @@
+/*
+    Jasmine BDD example spec
+    ========================
+
+    Built in matchers
+    -----------------
+    expect(x).toEqual(y); compares objects or primitives x and y and passes if they are equivalent
+    expect(x).toBe(y); compares objects or primitives x and y and passes if they are the same object
+    expect(x).toMatch(pattern); compares x to string or regular expression pattern and passes if they match
+    expect(x).toBeDefined(); passes if x is not undefined
+    expect(x).toBeUndefined(); passes if x is undefined
+    expect(x).toBeNull(); passes if x is null
+    expect(x).toBeTruthy(); passes if x evaluates to true
+    expect(x).toBeFalsy(); passes if x evaluates to false
+    expect(x).toContain(y); passes if array or string x contains y
+    expect(x).toBeLessThan(y); passes if x is less than y
+    expect(x).toBeGreaterThan(y); passes if x is greater than y
+    expect(function(){fn();}).toThrow(e); passes if function fn throws exception e when executed
+*/
+/*global $, jQuery,define,require,describe,afterEach,beforeEach,expect,it,waitsFor,runs */
+(function(){
+    'use strict';
+
+var SPEC_SETTINGS = {
+    name: 'video support',
+    module: 'extras/video'
+};
+describe(SPEC_SETTINGS.name, function(){
+
+    var mod;
+
+    beforeEach(function(){
+        this.addMatchers({
+            toBeArray: function(){
+                return this.actual.constructor === Array;
+            },
+            toContain: function(expected) {
+                if (typeof this.actual === 'string') {
+                    return this.actual.indexOf(expected) > -1;
+                }
+            }
+        });
+        waitsFor(function(){
+            return mod && typeof mod === 'object';
+        }, 'never got a ' + SPEC_SETTINGS.name + ' module', 1000);
+
+        require(window.testRequireOptions, [SPEC_SETTINGS.module], function(module) {
+                mod = module;
+        });
+    });
+
+    it('should have the ' + SPEC_SETTINGS.name + ' module', function() {
+        expect(mod).toBeDefined();
+    });
+
+    describe(SPEC_SETTINGS.name, function(){
+
+        it('should be able to use jQuery', function(){
+            expect(jQuery).toBeDefined();
+        });
+        it("should be able to use jQuery dialog", function() {
+            expect(jQuery.fn.dialog).toBeDefined();
+        });
+        it("can be initialised", function() {
+			mod.init();
+        });
+        it("can detect a vimeo link", function() {
+			$('<a href="https://vimeo.com/34864389">vimeo</a>')
+				.appendTo('body')
+				.click();
+        });
+    });
+});
+
+}());
